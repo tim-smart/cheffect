a05e379e17e3d6300a96bd549f1bf9abfa295c98
Tim Smart <hello@timsmart.co>
2025-12-16 02:19:34 +1300

    add padding bottom to recipes

--- src/routes/recipe/$id.tsx
+++ src/routes/recipe/$id.tsx
@@ -50,7 +50,7 @@ export function RecipeDetails({ recipe }: { recipe: Recipe }) {
   }
 
   return (
-    <div className="bg-gray-50">
+    <div className="pb-30">
       {/* Header */}
       <header className="bg-white border-b border-gray-200 sticky top-0 z-10">
         <div className="px-2 py-4">
