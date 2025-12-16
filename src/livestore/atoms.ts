ec7f99a69f717f60f5d8c28b20c03122b7c8674b
Tim Smart <hello@timsmart.co>
2025-12-16 22:00:20 +1300

    use makePersistedAdapter again

--- src/livestore/atoms.ts
+++ src/livestore/atoms.ts
@@ -1,13 +1,15 @@
 import { schema } from "./schema"
-import { makeSingleTabAdapter } from "@livestore/adapter-web"
+import { makePersistedAdapter } from "@livestore/adapter-web"
 import { unstable_batchedUpdates } from "react-dom"
 import LiveStoreWorker from "./livestore.worker?worker"
+import LiveStoreSharedWorker from "@livestore/adapter-web/shared-worker?sharedworker"
 import { AtomLivestore } from "@effect-atom/atom-livestore"
 import { useAtomSet } from "@effect-atom/atom-react"
 
-const adapter = makeSingleTabAdapter({
+const adapter = makePersistedAdapter({
   storage: { type: "opfs" },
   worker: LiveStoreWorker,
+  sharedWorker: LiveStoreSharedWorker,
 })
 
 export class Store extends AtomLivestore.Tag<Store>()("Store", {
