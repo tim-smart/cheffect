2c86ab5a698644adc08906acb400b0adae546e6a
Tim Smart <hello@timsmart.co>
2025-12-16 21:37:41 +1300

    use livestore single tab mode

--- src/livestore/atoms.ts
+++ src/livestore/atoms.ts
@@ -1,15 +1,13 @@
 import { schema } from "./schema"
-import { makePersistedAdapter } from "@livestore/adapter-web"
+import { makeSingleTabAdapter } from "@livestore/adapter-web"
 import { unstable_batchedUpdates } from "react-dom"
 import LiveStoreWorker from "./livestore.worker?worker"
-import LiveStoreSharedWorker from "@livestore/adapter-web/shared-worker?worker"
 import { AtomLivestore } from "@effect-atom/atom-livestore"
 import { useAtomSet } from "@effect-atom/atom-react"
 
-const adapter = makePersistedAdapter({
+const adapter = makeSingleTabAdapter({
   storage: { type: "opfs" },
   worker: LiveStoreWorker,
-  sharedWorker: LiveStoreSharedWorker as any,
 })
 
 export class Store extends AtomLivestore.Tag<Store>()("Store", {
