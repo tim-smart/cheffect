396e8c2cd9ea4a52cb4171ae6a9bbec6e06dd8a9
Tim Smart <hello@timsmart.co>
2025-12-18 12:04:16 +1300

    exclude api from sw

--- src/sw.ts
+++ src/sw.ts
@@ -5,6 +5,7 @@ import {
   precacheAndRoute,
 } from "workbox-precaching"
 import { NavigationRoute, registerRoute } from "workbox-routing"
+import { NetworkOnly } from "workbox-strategies"
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -16,6 +17,8 @@ cleanupOutdatedCaches()
 
 // to allow work offline
 registerRoute(new NavigationRoute(createHandlerBoundToURL("index.html")))
+
+registerRoute(({ url }) => url.pathname.startsWith("/api/"), new NetworkOnly())
 
 self.addEventListener("message", (event) => {
   if (!event.data) return
