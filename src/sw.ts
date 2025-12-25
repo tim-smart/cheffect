39adf117736e25aa44f3ec9d9705e2382c1a2fef
Tim Smart <hello@timsmart.co>
2025-12-25 16:14:54 +1300

    cache external images

--- src/sw.ts
+++ src/sw.ts
@@ -5,11 +5,18 @@ import {
   precacheAndRoute,
 } from "workbox-precaching"
 import { NavigationRoute, registerRoute } from "workbox-routing"
+import { CacheFirst } from "workbox-strategies"
 
 declare let self: ServiceWorkerGlobalScope
 
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
+
+// cache external images
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new CacheFirst(),
+)
 
 // clean old assets
 cleanupOutdatedCaches()
