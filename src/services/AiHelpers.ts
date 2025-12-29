b33fa6a3dd9227aa1806e971f5c7d398573b831b
Tim Smart <hello@timsmart.co>
2025-12-29 22:26:06 +1300

    try gpt-4.1 for groceries

--- src/services/AiHelpers.ts
+++ src/services/AiHelpers.ts
@@ -52,11 +52,7 @@ export class AiHelpers extends Effect.Service<AiHelpers>()("AiHelpers", {
         effort: "medium",
       },
     })
-    const groceryModel = yield* OpenAiLanguageModel.model("o4-mini", {
-      reasoning: {
-        effort: "low",
-      },
-    })
+    const groceryModel = yield* OpenAiLanguageModel.model("gpt-4.1")
     const proxy = yield* CorsProxy
 
     const recipeFromUrl = Effect.fn("AiHelpers.recipeFromUrl")(function* (
