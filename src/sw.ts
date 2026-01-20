4f531a8b9e176f8a85e6fb5c989e635a8e06975c
Tim Smart <hello@timsmart.co>
2026-01-20 11:26:46 +0000

    add runtime image cache

--- src/sw.ts
+++ src/sw.ts
@@ -5,6 +5,7 @@ import {
   precacheAndRoute,
 } from "workbox-precaching"
 import { NavigationRoute, registerRoute } from "workbox-routing"
+import { CacheableResponsePlugin } from "workbox-cacheable-response"
 import { CacheFirst } from "workbox-strategies"
 
 declare let self: ServiceWorkerGlobalScope
@@ -12,10 +13,15 @@ declare let self: ServiceWorkerGlobalScope
 // self.__WB_MANIFEST is default injection point
 precacheAndRoute(self.__WB_MANIFEST)
 
+const imageCacheName = "cheffect-images-v1"
+
 // cache external images
 registerRoute(
   ({ request }) => request.destination === "image",
-  new CacheFirst(),
+  new CacheFirst({
+    cacheName: imageCacheName,
+    plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })],
+  }),
 )
 
 // clean old assets
